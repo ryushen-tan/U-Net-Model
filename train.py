7935c9333f4098fb5d3628ea45307ef98f29106e
ryushen-tan <ryushen@gmail.com>
2024-11-12 22:22:19 -0500

    adjusted files

--- train.py
+++ train.py
@@ -1,11 +1,10 @@
-import torch 
+import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from model import UNET
-
 from utils import (
     load_checkpoint,
     save_checkpoint,
@@ -17,11 +16,11 @@ from utils import (
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
+BATCH_SIZE = 8
 NUM_EPOCHS = 3
 NUM_WORKERS = 2
-IMAGE_HEIGHT = 512  # Adjusted from 1024
-IMAGE_WIDTH = 1024  # Adjusted from 2041
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
 PIN_MEMORY = True
 LOAD_MODEL = False
 TRAIN_IMG_DIR = "data/train_images/"
@@ -37,7 +36,7 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
         targets = targets.float().unsqueeze(1).to(device=DEVICE)
 
         # forward
-        with torch.amp.autocast(device_type=DEVICE):
+        with torch.cuda.amp.autocast():
             predictions = model(data)
             loss = loss_fn(predictions, targets)
 
@@ -49,8 +48,6 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
 
         # update tqdm loop
         loop.set_postfix(loss=loss.item())
-
-
 
 
 def main():
@@ -66,7 +63,7 @@ def main():
                 max_pixel_value=255.0,
             ),
             ToTensorV2(),
-        ]
+        ],
     )
 
     val_transforms = A.Compose(
@@ -78,11 +75,11 @@ def main():
                 max_pixel_value=255.0,
             ),
             ToTensorV2(),
-        ]
+        ],
     )
 
     model = UNET(in_channels=3, out_channels=1).to(DEVICE)
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     train_loader, val_loader = get_loaders(
@@ -97,12 +94,12 @@ def main():
         PIN_MEMORY,
     )
 
-
-    scaler = torch.amp.GradScaler()
     if LOAD_MODEL:
-        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
 
-    scalar = torch.amp.GradScaler()
+
+    check_accuracy(val_loader, model, device=DEVICE)
+    scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(NUM_EPOCHS):
         train_fn(train_loader, model, optimizer, loss_fn, scaler)
@@ -110,17 +107,18 @@ def main():
         # save model
         checkpoint = {
             "state_dict": model.state_dict(),
-            "optimizer": optimizer.state_dict(),
+            "optimizer":optimizer.state_dict(),
         }
         save_checkpoint(checkpoint)
+
         # check accuracy
         check_accuracy(val_loader, model, device=DEVICE)
 
-
-        # print examples to folder
+        # print some examples to a folder
         save_predictions_as_imgs(
             val_loader, model, folder="saved_images/", device=DEVICE
         )
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,35 +1,33 @@
-import torch 
+import torch
 import torchvision
-from dataset import TeethDataset
+from dataset import TeethDataSet
 from torch.utils.data import DataLoader
 
 def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
     print("=> Saving checkpoint")
     torch.save(state, filename)
 
-def load_checkpoint(checkpoint, model, optimizer):
+def load_checkpoint(checkpoint, model):
     print("=> Loading checkpoint")
     model.load_state_dict(checkpoint["state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer"]) 
 
-    
 def get_loaders(
-        train_dir,
-        train_maskdir,
-        val_dir,
-        val_maskdir,
-        batch_size,
-        train_transform,
-        val_transform,
-        num_workers=4,
-        pin_memory=True,
-    ):
-    train_ds = TeethDataset(
+    train_dir,
+    train_maskdir,
+    val_dir,
+    val_maskdir,
+    batch_size,
+    train_transform,
+    val_transform,
+    num_workers=4,
+    pin_memory=True,
+):
+    train_ds = TeethDataSet(
         image_dir=train_dir,
         mask_dir=train_maskdir,
         transform=train_transform,
     )
-    
+
     train_loader = DataLoader(
         train_ds,
         batch_size=batch_size,
@@ -38,7 +36,7 @@ def get_loaders(
         shuffle=True,
     )
 
-    val_ds = TeethDataset(
+    val_ds = TeethDataSet(
         image_dir=val_dir,
         mask_dir=val_maskdir,
         transform=val_transform,
@@ -71,22 +69,25 @@ def check_accuracy(loader, model, device="cuda"):
             dice_score += (2 * (preds * y).sum()) / (
                 (preds + y).sum() + 1e-8
             )
+
     print(
-        f"Got {num_correct}/{num_pixels} with accuracy {num_correct/num_pixels*100:.2f}"
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
     )
     print(f"Dice score: {dice_score/len(loader)}")
     model.train()
 
-def save_predictions_as_imgs(loader, model, folder, device="cuda"):
-    loader, model, folder, device = "cuda"
-
+def save_predictions_as_imgs(
+    loader, model, folder="saved_images/", device="cuda"
+):
     model.eval()
     for idx, (x, y) in enumerate(loader):
         x = x.to(device=device)
         with torch.no_grad():
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
-        torchvision.utils.save_image(preds, f"{folder}/pred_{idx}.png")
+            torchvision.utils.save_image(
+            preds, f"{folder}/pred_{idx}.png"
+        )
         torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
 
     model.train()
