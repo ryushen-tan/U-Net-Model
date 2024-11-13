aac6119e63611b3a91d745649fb4f9f6618968da
ryushen-tan <ryushen@gmail.com>
2024-11-12 23:42:45 -0500

    feat: tested models and changed the block size

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from utils import (
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 NUM_EPOCHS = 3
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 160  # 1280 originally
